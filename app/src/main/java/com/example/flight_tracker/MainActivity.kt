da0cad4d9f8cbb0e8f3d77f6a61f4dc3c8600276
DOUMBOUYA FODE <47141813+fodedoumbouya@users.noreply.github.com>
2023-10-20 16:47:00 +0200

    features [x] clear the code

--- app/src/main/java/com/example/flight_tracker/MainActivity.kt
+++ app/src/main/java/com/example/flight_tracker/MainActivity.kt
@@ -1,45 +1,16 @@
 package com.example.flight_tracker;
 
-import android.app.Activity;
-import android.content.Intent
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.flight_tracker.pages.details.Details
-import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.maps.MapView
-import com.mapbox.mapboxsdk.maps.MapboxMap
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
-import com.mapbox.mapboxsdk.maps.Style
 
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var mapView: MapView
-
-//    private var mapView: MapView? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
-
-
         setContentView(R.layout.activity_main)
-        // Call the Maps activity
-//        startActivity(Intent(this, Details::class.java))
-//
-//        mapView = findViewById(R.id.mapView)
-//        mapView?.onCreate(savedInstanceState)
-//        mapView?.getMapAsync { mapboxMap ->
-//
-//            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
-//// Map is set up and the style has loaded. Now you can add data or make other map adjustments
-//
-//
-//            }
-//
-//        }
+
     }
 }
