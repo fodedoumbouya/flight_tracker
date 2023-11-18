cde84f8b5f3fd164f1f708baede7185d3d2b88fb
idricealy <idricemourtadhoi@gmail.com>
2023-11-18 23:45:49 +0100

    feat: add using file json if api is down

--- app/src/main/java/com/example/flight_tracker/data/DataManager.kt
+++ app/src/main/java/com/example/flight_tracker/data/DataManager.kt
@@ -1,5 +1,6 @@
 package com.example.flight_tracker.data
 
+import com.example.flight_tracker.commom.Utils
 import com.example.flight_tracker.network.OpenSkiApiService
 import com.example.flight_tracker.network.RequestListener
 
@@ -8,13 +9,22 @@ import com.example.flight_tracker.network.RequestListener
  */
 object DataManager {
 
-    suspend fun getFlights(
+    fun getFlights(
         airport: String,
         begin: Int,
         end: Int,
         isDeparture: Boolean
     ): RequestListener<*> {
 
-        return OpenSkiApiService.getFlights(airport, begin, end, isDeparture)
+        return when(val requestListener = OpenSkiApiService.getFlights(airport, begin, end, isDeparture)) {
+            // If response API is 500, we use sample data
+            is RequestListener.Error -> {
+                RequestListener.Success<String>(Utils.assetsJsonFileToJsonArray("flights_list_sample.json").toString())
+            }
+
+            else -> {
+                requestListener
+            }
+        }
     }
 }
